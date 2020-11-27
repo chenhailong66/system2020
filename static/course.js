4805c8a5788ed064b5c327f1e0ad954fd60fc92e
陈海龙 <1846989975@qq.com>
2020-11-27 22:17:47 +0800

    上一个追加提交

--- static/course.js
+++ static/course.js
@@ -8,39 +8,39 @@ async function renderList() {
 
     let tbodyEl = document.createElement("tbody");
     for (let item of data) {
-            let trEl = document.createElement("tr");
-            tbodyEl.append(trEl);
+        let trEl = document.createElement("tr");
+        tbodyEl.append(trEl);
 
-            let tdEl;
-            tdEl = document.createElement("td");
-            tdEl.innerText = item.name;
-            tdEl.className = "col-cou_name";
-            trEl.append(tdEl);
+        let tdEl;
+        tdEl = document.createElement("td");
+        tdEl.innerText = item.name;
+        tdEl.className = "col-cou_name";
+        trEl.append(tdEl);
 
-            tdEl = document.createElement("td");
-            tdEl.className = "col-cou_no";
-            tdEl.innerText = item.no;
-            trEl.append(tdEl);
+        tdEl = document.createElement("td");
+        tdEl.className = "col-cou_no";
+        tdEl.innerText = item.no;
+        trEl.append(tdEl);
 
-            tdEl = document.createElement("td");
-            tdEl.className = "col-place";
-            tdEl.innerText = item.place;
-            trEl.append(tdEl);
+        tdEl = document.createElement("td");
+        tdEl.className = "col-place";
+        tdEl.innerText = item.place;
+        trEl.append(tdEl);
 
-            tdEl = document.createElement("td");
-            tdEl.className = "col-time";
-            tdEl.innerText = item.time;
-            trEl.append(tdEl);
+        tdEl = document.createElement("td");
+        tdEl.className = "col-time";
+        tdEl.innerText = item.time;
+        trEl.append(tdEl);
 
-            tdEl = document.createElement("td");
-            tdEl.className = "";
-            tdEl.style.width = '1em'
-            trEl.append(tdEl);
+        tdEl = document.createElement("td");
+        tdEl.className = "";
+        tdEl.style.width = '1em'
+        trEl.append(tdEl);
 
-            tdEl = document.createElement("td");
-            tdEl.className = "ctrlbar";
-            tdEl.append(renderRecordCtrlbar(item));
-            trEl.append(tdEl);
+        tdEl = document.createElement("td");
+        tdEl.className = "ctrlbar";
+        tdEl.append(renderRecordCtrlbar(item));
+        trEl.append(tdEl);
     }
     let tableEl = document.querySelector("#course-table");
     document.querySelector("#course-table > tbody").remove();
@@ -55,7 +55,7 @@ function renderRecordCtrlbar(item) {
     editBtn.innerText = "修改";
 
     //放修改按钮的click事件
-    
+
     ctrlbarEl.append(editBtn);
 
     let delBtn = document.createElement("a");
