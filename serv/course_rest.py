f471a6e4f3e58ead0f39264a581b04720ddfe7a0
贺煊之 <1185419498@qq.com>
2020-12-07 22:41:30 +0800

    实现选课功能

--- serv/course_rest.py
+++ serv/course_rest.py
@@ -77,3 +77,40 @@ async def delete_courseplan(request):
         """, dict(courseplan_sn = courseplan_sn))
 
     return web.Response(text="", content_type="text/plain")
+
+#读取一个课程计划，所有选择该计划的学生111
+@web_routes.get("/api/course/{courseplan_sn:\d+}")
+async def get_courseplan_list(request):
+    courseplan_sn = request.match_info.get("courseplan_sn")
+    with db_block() as db:
+        db.execute("""
+        select stu_sn
+        from course_grade2
+        where courseplan_sn = %(courseplan_sn)s;
+        """,dict(courseplan_sn = courseplan_sn))
+        data = list(asdict(r) for r in db)
+
+    return web.Response(text=json_dumps(data), content_type="application/json")
+@web_routes.delete("/api/course/{stu_sn:\d+}/{courseplan_sn:\d+}")
+async def delete_courseplan(request):
+    stu_sn = int(request.match_info.get("stu_sn"))
+    courseplan_sn = int(request.match_info.get("courseplan_sn"))
+    with db_block() as db:
+        db.execute("""
+        DELETE FROM course_grade2 WHERE stu_sn=%(stu_sn)s and courseplan_sn = %(courseplan_sn)s;
+        """, dict(stu_sn = stu_sn,courseplan_sn = courseplan_sn))
+    #data = list(asdict(stu_sn = stu_sn,courseplan_sn = courseplan_sn))
+    return web.Response(text='', content_type="text/plain")
+
+@web_routes.post("/api/course/choose_course")
+async def new_courseplan(request):
+    choose_course = await request.json()
+    choose_course['stu_sn'] = int(choose_course['stu_sn'])
+    choose_course['courseplan_sn'] = int(choose_course['courseplan_sn'])
+    #将传入数据中的课程名，改成课程序号
+    with db_block() as db:
+        db.execute("""
+        INSERT INTO course_grade2 (stu_sn, courseplan_sn, grade)
+        VALUES(%(stu_sn)s, %(courseplan_sn)s,%(grade)s)
+        """, choose_course)
+    return web.Response(text=json_dumps(choose_course), content_type="application/json")   
--- static/course.js
+++ static/course.js
@@ -1,19 +1,20 @@
 //设置当前学期，和实现更改学期操作
 
 let current_term = '2020-2021'
+
 function change_term() {
     let inputEl = document.getElementById("current_term");
     let addTask = () => {
-        current_term = inputEl.value;
-        renderList();
-    }
-    //按ENTER键执行插入操作
-    inputEl.onkeypress = (e) => {
-        if (e.key === "Enter") {
-            addTask();
+            current_term = inputEl.value;
+            renderList();
         }
-    }
-    //点击加号执行插入操作
+        //按ENTER键执行插入操作
+    inputEl.onkeypress = (e) => {
+            if (e.key === "Enter") {
+                addTask();
+            }
+        }
+        //点击加号执行插入操作
     let addEl = document.getElementById("change_termbtn");
     addEl.onclick = (e) => {
         addTask();
@@ -94,7 +95,7 @@ function renderRecordCtrlbar(item) {
         openComfirmationDialog({
             message: `确定要删除在${item.place} ${item.time} ${item.name}这节课吗？`,
             onOk: () => {
-                (async () => {
+                (async() => {
                     let response = await fetch(`/api/course/${item.courseplan_sn}`, {
                         method: "DELETE",
                     });
@@ -128,12 +129,12 @@ async function openEditDialog(item) {
     //如果item不为空，显示修改页面
     if (item) {
         dialogTitle.innerText = `修改课程计划信息`;
-        form.elements.courseplan_sn.value = item.courseplan_sn ?? null;
+        form.elements.courseplan_sn.value = item.courseplan_sn ? ? null;
         form.elements.term.value = current_term;
-        form.elements.no.value = item.no ?? "";
-        form.elements.name.value = item.name ?? "";
-        form.elements.place.value = item.place ?? "";
-        form.elements.time.value = item.time ?? "";
+        form.elements.no.value = item.no ? ? "";
+        form.elements.name.value = item.name ? ? "";
+        form.elements.place.value = item.place ? ? "";
+        form.elements.time.value = item.time ? ? "";
     }
     //否则显示新建学生页面
     else {
@@ -162,10 +163,29 @@ async function renderEditDialog() {
     };
 
 
-    //点击学生选课按钮出现选课界面
+    //点击学生选课，打开选课界面
+
+
     let choose_dialog = document.querySelector(".select_course_grade_dialog")
     let choosecoursebtn = document.querySelector(".paper #choose-btn");
-    //这里实现具体功能
+    choosecoursebtn.onclick = (e) => {
+        //打开界面
+        ;
+        if (choose_dialog.classList.contains("open")) {
+            choose_dialog.classList.remove("open");
+        } else {
+            choose_dialog.classList.add("open");
+        }
+        //绘制页面内容
+        choose_course_Dialog();
+
+    };
+
+    //实现保存按钮和退出按钮
+    let choose_course_backbtn = document.querySelector("#choose_course_back-btn");
+    choose_course_backbtn.onclick = (e) => {
+        choose_dialog.classList.remove("open");
+    }
 
 
 
@@ -197,7 +217,7 @@ async function renderEditDialog() {
         //当数据没有默认序号是即为新建
         if (!data.courseplan_sn) {
             // 异步执行POST请求操作
-            (async () => {
+            (async() => {
                 let response = await fetch("/api/course", {
                     method: "POST",
                     headers: {
@@ -217,7 +237,7 @@ async function renderEditDialog() {
         //否则就为修改 
         else {
             // 异步执行PUT请求操作
-            (async () => {
+            (async() => {
                 let response = await fetch(`/api/course/${data.courseplan_sn}`, {
                     method: "PUT",
                     headers: {
@@ -268,6 +288,162 @@ async function openComfirmationDialog({ message, onOk, onCancel }) {
     messageEl.innerText = message;
 
     dialog.classList.add("open");
+}
+
+
+
+let choose_course_term = '2020-2021';
+
+async function change_choose_term() {
+    let inputEl = document.getElementById("choose_course_term");
+    let addTask = () => {
+            choose_course_term = inputEl.value;
+            choose_course_Dialog();
+
+        }
+        //按ENTER键执行插入操作
+    inputEl.onkeypress = (e) => {
+            if (e.key === "Enter") {
+                addTask();
+            }
+        }
+        //点击加号执行插入操作
+    let addEl = document.getElementById("change_choose_termbtn");
+    addEl.onclick = (e) => {
+        addTask();
+    }
+}
+
+
+//学生选课页面的绘制
+async function choose_course_Dialog() {
+    let response = await fetch(`/api/course/list`);
+    if (!response.ok) {
+        console.error(response);
+        return;
+    }
+
+    let data = await response.json();
+    let tbodyEl = document.createElement("tbody");
+    for (let item of data) {
+        if (item.term === choose_course_term) {
+            let trEl = document.createElement("tr");
+            tbodyEl.append(trEl);
+
+            let tdEl;
+            tdEl = document.createElement("td");
+            tdEl.innerText = item.name;
+            tdEl.className = "col-cou_name";
+            trEl.append(tdEl);
+
+            tdEl = document.createElement("td");
+            tdEl.className = "col-cou_no";
+            tdEl.innerText = item.no;
+            trEl.append(tdEl);
+
+            tdEl = document.createElement("td");
+            tdEl.className = "col-place";
+            tdEl.innerText = item.place;
+            trEl.append(tdEl);
+
+            tdEl = document.createElement("td");
+            tdEl.className = "col-time";
+            tdEl.innerText = item.time;
+            trEl.append(tdEl);
+
+            //添加选课和取消按钮
+            tdEl = document.createElement("td");
+            tdEl.className = "choose_thiscourse";
+            tdEl.append(choose_and_delete(item));
+            trEl.append(tdEl);
+        }
+    }
+
+    let tableEl = document.querySelector("#choose_course-table");
+    document.querySelector("#choose_course-table > tbody").remove();
+    tableEl.append(tbodyEl);
+
+
+    //一个课程计划全部选择同学的名单
+    for (let item of data) {
+        if (item.term === choose_course_term) {
+
+            let choose_course = document.querySelector(`.plan${item.courseplan_sn}.btn`);
+            let choose_course_studentname = document.querySelector("#choose_course_studentname");
+            let response1 = await fetch(`/api/course/${item.courseplan_sn}`);
+            if (!response1.ok) {
+                console.error(response1);
+                return;
+            }
+            let datas = await response1.json();
+            for (let item3 of datas) {
+                //如果该学生已经选过该课程则不可以再次选择
+                if (String(item3.stu_sn) === choose_course_studentname.value) {
+                    choose_course.disabled = true;
+                }
+            }
+        }
+    }
+}
+//===================================================实现学生选课界面内的选课和取消按钮===================
+function choose_and_delete(item) {
+    let ctrlbarEl = document.createElement("div");
+
+    let choose_course = document.createElement("button");
+    choose_course.className = `plan${item.courseplan_sn} btn`;
+    choose_course.innerText = "选择";
+
+
+
+    let choose_course_studentname = document.querySelector("#choose_course_studentname");
+    //点击确认选课
+    choose_course.onclick = (e) => {
+        let data = {
+            stu_sn: choose_course_studentname.value,
+            courseplan_sn: item.courseplan_sn,
+            grade: 0.0,
+        };
+        (async() => {
+            let response = await fetch(`/api/course/choose_course`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json;charset=utf-8",
+                },
+                body: JSON.stringify(data),
+            });
+
+            if (!response.ok) {
+                console.error(response);
+                return;
+            }
+        })();
+        choose_course.disabled = true;
+    }
+
+    ctrlbarEl.append(choose_course);
+
+    let delBtn = document.createElement("a");
+    delBtn.className = "btn";
+    delBtn.innerText = "取消";
+    delBtn.onclick = (e) => {
+        (async() => {
+            let stu_sn = choose_course_studentname.value;
+            let courseplan_sn = item.courseplan_sn;
+            let response = await fetch(`/api/course/${stu_sn}/${courseplan_sn}`, {
+                method: "DELETE",
+            });
+
+            if (!response.ok) {
+                console.error(response);
+                return;
+            }
+        })();
+        choose_course.disabled = false;
+    }
+
+    ctrlbarEl.append(delBtn);
+
+    return ctrlbarEl;
 }
 
 
